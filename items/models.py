d7f26688db1c77718585ac03cbadaa8ee5015831
Abdullah Turan <abdullah_turan@hotmail.com>
2020-06-02 23:17:41 +0300

    first commit

---
+++ items/models.py
@@ -0,0 +1,66 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    COLORS = (
+        ('S', 'success'),
+        ('P', 'primary'),
+        ('I', 'info'),
+        ('W', 'warning'),
+        ('D', 'danger'),
+        ('A', 'dark'),
+        ('E', 'secondary'),
+        ('L', 'light')
+    )
+
+    ICONS = (
+        ('T', 'mobile-alt'),
+        ('B', 'book'),
+        ('M', 'music'),
+        ('C', 'car'),
+        ('H', 'home'),
+        ('A', 'basketball-ball'),
+        ('E', 'headphones'),
+        ('D', 'baby-carriage'),
+    )
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=1, choices=COLORS, default='P')
+    icon = models.CharField(max_length=12, choices=ICONS, default='B')
+
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name    
+
+
+class Item(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='items')
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=250)
+    price = models.FloatField()
+    picture = models.ImageField(upload_to='item_pictures/', default="item_pictures/default.jpg")
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, related_name='items')
+
+    slug = models.SlugField(unique=True, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super(Item, self).save(*args, **kwargs)
+    def __str__(self):
+        return self.name
+    
+
+class ItemRental(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
+    hirer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    fulfilled = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,PasswordResetForm,UserChangeForm,ReadOnlyPasswordHashField,ReadOnlyPasswordHashWidget
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib.auth.hashers import (
+    UNUSABLE_PASSWORD_PREFIX, identify_hasher,
+)
+class RealDateInput(forms.DateInput):
+    input_type = "date"
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = UserProfile
+        fields = UserCreationForm.Meta.fields +  (
+        "password1","password2", "email","first_name", "last_name","telephone", "birth_date", "location", "profile_picture")
+        widgets = {"birthday": RealDateInput}
+class UserLoginForm(AuthenticationForm):
+    remember_me = forms.BooleanField(required=False)
+
+
+class ProfileUpdateForm(UserChangeForm,ReadOnlyPasswordHashField,ReadOnlyPasswordHashWidget):
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(""),
+    )     
+    template_name = 'user/profile_edit.html'
+
+  
+    class Meta(UserChangeForm):
+        model = UserProfile
+        fields = [
+            'first_name', 'last_name', 'email', 'telephone', 'birth_date', 'location', 'profile_picture']
